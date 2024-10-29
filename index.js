b71da6fff4f6ba2445b7659b88f3937ab2188fa1
obedposadas <obedposadas92@gmail.com>
2024-10-29 01:08:25 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import pg from "pg";
+
+import { config } from "dotenv";
+
+config();
+// Conectar a la base de datos
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+
+// Crear una nueva aplicación Express
+const app = express();
+
+// Definir un puerto para nuestro servidor
+const port = 3000 || process.env.PORT;
+
+// Definir una ruta de prueba
+app.get("/", (req, res) => {
+  res.send("¡Hola Mundo!");
+});
+
+app.get("/ping", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  return res.json(result.rows[0]);
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
